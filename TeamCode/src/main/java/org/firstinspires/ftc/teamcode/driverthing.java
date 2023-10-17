89bc9830ac3ff2e09eb1e9c398c3d661c7eedcb6
Orange-4998 <home0ne350@gmail.com>
2023-10-17 11:34:38 -0500

    Save data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverthing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverthing.java
@@ -7,6 +7,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 @TeleOp(name="DRIVETRAIN_ONLY", group="Driver OP")
 public class driverthing extends LinearOpMode {
 
@@ -19,6 +32,8 @@ public class driverthing extends LinearOpMode {
     public DcMotor fr;
     public DcMotor bl;
     public DcMotor br;
+    public DcMotor classtestmotor;
+
     //public DcMotor E;
    // public ColorSensor color_sensor;
 
@@ -26,11 +41,33 @@ public class driverthing extends LinearOpMode {
 
 
 
+    public static final String TAG = "Vuforia VuMark Sample";
+    WebcamName webcamName;
+    OpenCvWebcam webcam;
+    OpenCvPipeline pipeline;
 
+    @Override public void runOpMode() {
 
-    @Override
-    public void runOpMode() {
+        /*
+         * Retrieve the camera we are to use.
+         */
+        webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);        // OR...  Do Not Activate the Camera Monitor View, to save power
+        pipeline = new opencvpipelines();
+        webcam.setPipeline(pipeline);
 
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(1920,1080, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
 
 
 
@@ -41,7 +78,7 @@ public class driverthing extends LinearOpMode {
         fr= hardwareMap.get(DcMotor.class, "FR");
         bl= hardwareMap.get(DcMotor.class, "BL");
         br= hardwareMap.get(DcMotor.class, "BR");
-
+        classtestmotor = hardwareMap.get(DcMotor.class, "classtestmotor");
        // E = hardwareMap.get(DcMotor.class, "E");
 
         //grabber = hardwareMap.get(Servo.class, "grab");
@@ -50,6 +87,8 @@ public class driverthing extends LinearOpMode {
         fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        classtestmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         //E.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //E.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
@@ -57,11 +96,15 @@ public class driverthing extends LinearOpMode {
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        classtestmotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         fl.setDirection(DcMotor.Direction.REVERSE);
         fr.setDirection(DcMotor.Direction.FORWARD);
         bl.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.FORWARD);
+        classtestmotor.setDirection(DcMotor.Direction.FORWARD);
+
 
         // runs the moment robot is initialized
         waitForStart();
@@ -141,9 +184,10 @@ public class driverthing extends LinearOpMode {
 
 
     void move(){
-        double horizontal = -gamepad1.left_stick_x*.5;   // this works so dont question it
-        double vertical = gamepad1.left_stick_y*.5;
-        double turn = -gamepad1.right_stick_x*2/3;
+        classtestmotor.setPower(1.0);
+        double horizontal = gamepad1.left_stick_x*.5;   // this works so dont question it
+        double vertical =-gamepad1.left_stick_y*.5;
+        double turn = gamepad1.right_stick_x*2/3;
         //  E.setPower(gamepad1.left_stick_y);
         fl.setPower((Range.clip((vertical + horizontal + turn), -1, 1))/**powersetterr*/);
         fr.setPower((Range.clip((vertical - horizontal - turn), -1, 1))/**powersetterr*/);
