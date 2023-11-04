e036b2fd14bb0532d0ff62c538f192d875b4cc77
RishiSandrana <rsandrana@cps.edu>
2023-11-04 14:11:49 -0500

    updated code from comp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverthing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driverthing.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -23,66 +23,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name="DRIVETRAIN_ONLY", group="Driver OP")
-public class driverthing extends LinearOpMode {
+public class driverthing extends OpMode {
+    //private final ElapsedTime runtime = new ElapsedTime();
+    // public Servo grabber;
+    //  double powersetterr = 1;
 
-    // Declare OpMode members.
-    private final ElapsedTime runtime = new ElapsedTime();
-   // public Servo grabber;
-    double powersetterr = 1;
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor bl;
+    DcMotor br;
+    DcMotor intakeMotor;
 
-    public DcMotor fl;
-    public DcMotor fr;
-    public DcMotor bl;
-    public DcMotor br;
-    public DcMotor intakeMotor;
-
-    public DcMotor linearextenderLeft;
-    public DcMotor linearextenderRight;
+    DcMotor linearextenderLeft;
+    DcMotor linearextenderRight;
     //public Servo paperAirplane;
-    public Servo extenderRotator;
-    public Servo extenderPlacer;
-
-
-
-    //public DcMotor E;
-   // public ColorSensor color_sensor;
-
-
-
-
-
-    public static final String TAG = "Vuforia VuMark Sample";
-    WebcamName webcamName;
-    OpenCvWebcam webcam;
-    OpenCvPipeline pipeline;
-
+    Servo extenderRotator;
+    Servo extenderPlacer;
     Boolean intake_running = false;
-
-     //final double MAX_LS_HEIGHT = 50;
-    @Override public void runOpMode() {
-
-        /*
-         * Retrieve the camera we are to use.
-         */
-        // webcamName = hardwareMap.get(WebcamName.class, "webcam");
-        //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        //webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);        // OR...  Do Not Activate the Camera Monitor View, to save power
-        //pipeline = new opencvpipelines();
-        //webcam.setPipeline(pipeline);
-
-        //webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        //{
-        // @Override
-        //  public void onOpened()
-        //{
-        //   webcam.startStreaming(1920,1080, OpenCvCameraRotation.UPRIGHT);
-        //}
-
-        // @Override
-        //public void onError(int errorCode) {}
-        // });
-
-
+    @Override
+    public void init() {
         fl = hardwareMap.get(DcMotor.class, "FL");
         fr = hardwareMap.get(DcMotor.class, "FR");
         bl = hardwareMap.get(DcMotor.class, "BL");
@@ -92,8 +51,8 @@ public class driverthing extends LinearOpMode {
         linearextenderRight = hardwareMap.get(DcMotor.class, "linearextenderRight");
 
         // paperAirplane = hardwareMap.get(Servo.class, "paperAirplane");
-         extenderRotator = hardwareMap.get(Servo.class, "extenderRotator");
-         extenderPlacer = hardwareMap.get(Servo.class, "extenderPlacer");
+        extenderRotator = hardwareMap.get(Servo.class, "extenderRotator");
+        extenderPlacer = hardwareMap.get(Servo.class, "extenderPlacer");
 
         // E = hardwareMap.get(DcMotor.class, "E");
 
@@ -104,24 +63,15 @@ public class driverthing extends LinearOpMode {
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      /*  linearextenderLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        linearextenderLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        linearextenderRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        linearextenderRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        //E.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-       */
-        //E.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-   //     linearextenderLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-     //   linearextenderRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //     linearextenderLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //   linearextenderRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
 
@@ -131,7 +81,7 @@ linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         br.setDirection(DcMotor.Direction.REVERSE);
         intakeMotor.setDirection(DcMotor.Direction.REVERSE);
         linearextenderLeft.setDirection(DcMotor.Direction.REVERSE);
-      //  linearextenderLeft.setDirection(DcMotor.Direction.FORWARD);
+        //  linearextenderLeft.setDirection(DcMotor.Direction.FORWARD);
 
 
 
@@ -139,106 +89,112 @@ linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         final double CENTIMETERS_PER_TICK = 1 / TICKS_PER_CENTIMETER;
 
         final double MAX_SLIDE_HEIGHT = 50;
+    }
+
+    // Declare OpMode members.
+
+
+
+    //public DcMotor E;
+   // public ColorSensor color_sensor;
+/*
+
+
+
+
+    public static final String TAG = "Vuforia VuMark Sample";
+    WebcamName webcamName;
+    OpenCvWebcam webcam;
+    OpenCvPipeline pipeline;
+
+ */
+
+  //  Boolean intake_running = false;
+
+     //final double MAX_LS_HEIGHT = 50;
+    @Override
+    public void loop() {
 
 
         // runs the moment robot is initialized 136
-        waitForStart();
-        runtime.reset();
+        //    waitForStart();
+        //    runtime.reset();
 
 
-        while (opModeIsActive()) {
+          move();
 
-            move();
-            if (gamepad1.dpad_left) {
-                telemetry.addData("accessed: stopintake", "0");
-                stopIntake();
-
-            }
-            if (gamepad1.dpad_right){
-                telemetry.addData("accessed: startintake", "0");
-
-                startIntake();
-
-            }
-            //  if (gamepad1.y) {
-            //  linearextenderLeft.setTargetPosition(50);
-            //    linearextenderRight.setTargetPosition(-50);
-            //    linearextenderLeft.setPower(1);
-            //    linearextenderRight.setPower(1);
+        if (gamepad1.dpad_left) {
+            telemetry.addData("accessed: stopintake", "0");
 
 
-            //  }
+          //  stopIntake();
+            intakeMotor.setPower(0);
+
         }
-      /*  if (gamepad1.a) {
-            linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        if (gamepad1.dpad_right) {
+            telemetry.addData("accessed: startintake", "0");
 
-            linearextenderLeft.setPower(0.5);
-            linearextenderRight.setPower(0.5);
+           // startIntake();
+intakeMotor.setPower(0.75);
         }
+        if (gamepad1.dpad_up) {
+            intakeMotor.setPower(-0.75);
+        }
+        //  if (gamepad1.y) {
+        //  linearextenderLeft.setTargetPosition(50);
+        //    linearextenderRight.setTargetPosition(-50);
+        //    linearextenderLeft.setPower(1);
+        //    linearextenderRight.setPower(1);
+
+
+        //  }
+
         if (gamepad1.y) {
-            linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            linearextenderLeft.setPower(-0.5);
-            linearextenderRight.setPower(-0.5);
+            telemetry.addData("accessed: lin_ex", "0");
+
+         //   linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //    linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            linearextenderRight.setPower(0.5);
+            linearextenderLeft.setPower(0.5);
         }
-        telemetry.addData("Slideheightleft", linearextenderLeft.getCurrentPosition());
-        telemetry.addData("Slideheightright", linearextenderRight.getCurrentPosition());*/
-         /*   if (gamepad1.y) {
-                linearextenderRight.setTargetPosition((int)TICKS_PER_CENTIMETER*50);
-                linearextenderLeft.setTargetPosition((int)TICKS_PER_CENTIMETER*50);
-
-                linearextenderLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                linearextenderRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
-            if (gamepad1.a) {
-                linearextenderRight.setTargetPosition((int)TICKS_PER_CENTIMETER*0);
-                linearextenderLeft.setTargetPosition((int)TICKS_PER_CENTIMETER*0);
-
-                linearextenderLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                linearextenderRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            }
-
-          */
-            if (gamepad1.y) {
-                telemetry.addData("accessed: lin_ex", "0");
-
-                linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-                linearextenderRight.setPower(0.5);
-                linearextenderLeft.setPower(0.5);
-            }
+        else {
+            linearextenderLeft.setPower(0);
+            linearextenderRight.setPower(0);
+        }
         if (gamepad1.a) {
             telemetry.addData("accessed: lin_ex2", "0");
 
-            linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+       //     linearextenderLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      //      linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
             linearextenderRight.setPower(-0.5);
             linearextenderLeft.setPower(-0.5);
         }
+        else {
+            linearextenderLeft.setPower(0);
+            linearextenderRight.setPower(0);
+        }
 
-           // if (gamepad1.dpad_down) {
-                //launch();
-           // }
-            if (gamepad1.right_bumper) {
-                 rotateBox();
+        // if (gamepad1.dpad_down) {
+        //launch();
+        // }
+        if (gamepad1.right_bumper) {
+            rotateBox();
 
-            }
-            if (gamepad1.left_bumper) {
-                   unrotateBox();
+        }
+        if (gamepad1.left_bumper) {
+            unrotateBox();
 
-            }
-            if (gamepad1.left_trigger > 0.3) {
-                 open();
+        }
+        if (gamepad1.b) {
+            open();
 
-            }
-            if (gamepad1.right_trigger > 0.3) {
-                  close();
+        }
+        if (gamepad1.x) {
+            close();
 
-            }
+        }
 
 
 
@@ -299,22 +255,23 @@ linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             linearextenderLeft.setPower(power);
         }*/
     //}
+
     void rotateBox(){
         extenderRotator.setPosition(0.3);
-        sleep(100);
+       // sleep(100);
     }
     void unrotateBox(){
         extenderRotator.setPosition(0.7);
-        sleep(100);
+    //    sleep(100);
     }
     void open(){
         extenderPlacer.setPosition(0.3);
-        sleep(100);
+   //     sleep(100);
     }
    void close(){
 
         extenderPlacer.setPosition(0.0);
-       sleep(100);
+   //    sleep(100);
     }
 
    // void launch() {
@@ -390,7 +347,7 @@ linearextenderRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         //intakeMotor.setPower(1.0);
         double horizontal = gamepad1.left_stick_x*.5;   // this works so dont question it
         double vertical =-gamepad1.left_stick_y*.5;
-        double turn = gamepad1.right_stick_x*2/3;
+        double turn = -gamepad1.right_stick_x*2/3;
         //  E.setPower(gamepad1.left_stick_y);
         fl.setPower((Range.clip((vertical + horizontal + turn), -1, 1))/**powersetterr*/);
         fr.setPower((Range.clip((vertical - horizontal - turn), -1, 1))/**powersetterr*/);
